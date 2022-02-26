75c0cd886ba253a955e0f65fe681919369fca52e
Artem Flex <AMaAlekseev@stud.kantiana.ru>
2022-02-26 18:37:44 +0200

    Task 2

---
+++ code/task 2.php
@@ -0,0 +1,12 @@
+<?php
+
+$int1 = 12345;
+echo $int1;
+$int2 = 54321;
+echo $int2;
+$float1 = 67.89;
+echo (6+6);
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo ($last_month - $this_month);
