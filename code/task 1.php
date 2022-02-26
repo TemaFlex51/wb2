d4a284b30850925ef5dc1c902642e26737a8ca0d
Artem Flex <AMaAlekseev@stud.kantiana.ru>
2022-02-26 18:31:27 +0200

    Task 1

---
+++ code/task 1.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Imagine a lot of code here */
+$very_bad_unclear_name="15 chicken wings";
+
+// Write your code here:
+$order = &$very_bad_unclear_name;
+$order .= " and two number nine, ...";
+
+//// Don't change the line below
+
+echo "<br>Your order is:$very_bad_unclear_name.";
